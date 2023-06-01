24e7b10bbaa8ce5bcc59b8b6c2c0b1cb21527282
+ <jkunins95@gmail.com>
2023-06-01 12:25:31 -0400

    array filter lab complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+//Code your solution here
+
+function findMatching(drivers, sought) {
+    return drivers.filter(
+        (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+    );
+}
+
+function fuzzyMatch(drivers, testString) {
+    return drivers.filter(
+        (possibleMatch) =>
+            possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+}
+
+function matchName(drivers, soughtName) {
+    return drivers.filter((record) => record.name === soughtName);
+}
